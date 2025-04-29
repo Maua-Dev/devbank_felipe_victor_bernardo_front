afa1fa8aceb2b19ecf03725d71b006f889d80b49
Victor Gasperi <victorgasperi@gmail.com>
2025-04-28 21:15:46 -0300

    feat(transactions): tela finalizada

--- src/components/TransactionCard.tsx
+++ src/components/TransactionCard.tsx
@@ -12,11 +12,11 @@ function TransactionCard({
   transactionType,
 }: Props) {
   return (
-    <div>
-      <p>Valor: {transactionValue}</p>
-      <p>Data: {transactionDate}</p>
-      <p>Saldo atual: {currentBalance}</p>
-      <p>Tipo: {transactionType}</p>
+    <div className="bg-[#0073E6]/30 text-xl text-[#0073E6] w-full p-5 rounded-lg">
+      <p>Valor: R$ {transactionValue}</p>
+      <p>Data: {new Date(transactionDate).toLocaleDateString()}</p>
+      <p>Saldo atual: R$ {currentBalance}</p>
+      <p>Tipo: {transactionType === `deposit` ? `Depósito` : `Saque`}</p>
     </div>
   );
 }
--- src/pages/TransitionsHistory.tsx
+++ src/pages/TransitionsHistory.tsx
@@ -1,17 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import TransactionCard from "../components/TransactionCard";
 import { useNavigate } from "react-router-dom";
 
-export interface Transaction {
-  transactionValue: number;
-  transactionDate: number; // timestamp
-  currentBalance: number;
-  transactionType: string;
+type HistoryResponseType =  {
+  type: string;
+  value: number;
+  current_balance: number;
+  timestamp: number; // timestamp
 }
 
 export function TransitionsHistory() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactions, setTransactions] = useState<HistoryResponseType[]>([]);
+
+  async function getTransactions() {
+
+    try {
+
+      const response = await ( await fetch(`https://r2tcz6zsokynb72jb6o4ffd5nm0ryfyz.lambda-url.us-west-2.on.aws/history`)).json();
+
+      setTransactions(response.all_transactions as HistoryResponseType[])
+
+      console.log(transactions)
+
+    } catch (err) {
+      
+      console.log(err)
+
+    }
+
+  } 
+
+  useEffect(  () => {
+
+    // const get_transactions = async () => {
+    //   const result = await (await fetch(`https://r2tcz6zsokynb72jb6o4ffd5nm0ryfyz.lambda-url.us-west-2.on.aws/history`) ).json()
+    //   console.log(result)
+    // }
+    
+    // get_transactions()
+
+    getTransactions();
+
+  }, [])
 
   const navigate = useNavigate();
 
@@ -24,29 +55,33 @@ export function TransitionsHistory() {
       </div>
 
       <div className="flex flex-col mx-5 mb-2 gap-2 justify-center items-center">
-        {/* {transactions.map( transaction => 
+        { transactions &&
+        transactions.map((transaction) => (
+          <TransactionCard
+            transactionValue={transaction.value}
+            transactionDate={transaction.timestamp}
+            currentBalance={transaction.current_balance}
+            transactionType={transaction.type}
+          />
+        ))
+        }
 
-                        <TransactionCard transactionValue={transaction.transactionValue} transactionDate={transaction.transactionDate} currentBalance={transaction.currentBalance} transactionType={transaction.transactionType} />
-
-                    )} */}
-
-        <div className="bg-[#0073E6]/30 text-xl text-[#0073E6] w-full p-5 rounded-lg">
+        {/* <div className="bg-[#0073E6]/30 text-xl text-[#0073E6] w-full p-5 rounded-lg">
           <p>asd</p>
         </div>
 
         <div className="bg-[#0073E6]/30 text-xl text-[#0073E6] w-full p-5 rounded-lg">
           <p>asd</p>
-        </div>
-        
+        </div> */}
       </div>
 
       <div className="flex h-full items-end justify-center m-2">
         <button
-        className="transition bg-blue-500 text-white font-bold rounded-lg p-2 w-[150px]"
-        onClick={() => navigate("/")}
-      >
-        Voltar
-      </button>
+          className="transition bg-blue-500 text-white font-bold rounded-lg p-2 w-[150px]"
+          onClick={() => navigate("/")}
+        >
+          Voltar
+        </button>
       </div>
     </div>
   );
