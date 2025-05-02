88da661870ef2c4559ba24ca5a6e23a172503d50
Sakaef03 <fosakae@icloud.com>
2025-05-02 17:00:57 -0300

    feat: converte logo do header para botão

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,25 @@
-import { Component } from "react";
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
-
-type HeaderProps = {
-    name: string;
-    agency: string;
-    account: string;
+interface HeaderProps {
+  name: string;
+  agency: string;
+  account: string;
 }
 
-class Header extends Component<HeaderProps> {
-
-  render() {
-    return (
-      <nav className="bg-blue-700 h-[100px] relative flex items-center pl-5">
-        <p className="font-bold text-6xl text-white">DevBank</p>
-        <div className="absolute right-10 bg-white rounded-lg h-[75px] w-[200px] px-[5px] text-blue-700 flex flex-col justify-center">
-            <p>Nome: {this.props.name}</p>
-            <p>Agência: {this.props.agency}</p>
-            <p>Conta: {this.props.account}</p>
-        </div>
-      </nav>
-    );
-  }
-}
+const Header: FC<HeaderProps> = ({ name, agency, account }) => {
+  const navigate = useNavigate();
+  return (
+    <nav className="bg-blue-700 h-[100px] relative flex items-center pl-5">
+      {/* <p className="font-bold text-6xl text-white">DevBank</p> */}
+      <button className="font-bold text-6xl text-white" onClick={() => navigate("/")}>DevBank</button>
+      <div className="absolute right-10 bg-white rounded-lg h-[75px] w-[200px] px-[5px] text-blue-700 flex flex-col justify-center">
+        <p>Nome: {name}</p>
+        <p>Agência: {agency}</p>
+        <p>Conta: {account}</p>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
